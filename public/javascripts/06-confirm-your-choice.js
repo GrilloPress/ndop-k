1a39f6cd868a7ab1ac9b50c46c7b4517d327f286
grillopress <grillopress@gmail.com>
2018-02-20 12:36:12 +0000

    Update prototype K flash and question wording

--- public/javascripts/06-confirm-your-choice.js
+++ public/javascripts/06-confirm-your-choice.js
@@ -9,9 +9,9 @@ function preferenceChecker() {
 
   // This is now an ugly hack...
 
-  var researchPrefText = ( researchPref == "true"  ) ?  "<p>Your health data <strong>will</strong> be shared for research purposes | <a href='/F/app/05-make-your-choice' onclick='history.go(-1); return false;'>Edit <span class='util-visuallyhidden'>your research preference</span></a></p>" : "<p>Your health data <strong>will not</strong> be shared for research purposes | <a href='/F/app/05-make-your-choice' onclick='history.go(-1); return false;'>Edit</a></p>";
+  var researchPrefText = ( researchPref == "true"  ) ?  "<p>Your health data <strong>will</strong> be used for research purposes | <a href='/F/app/05-make-your-choice' onclick='history.go(-1); return false;'>Edit <span class='util-visuallyhidden'>your research preference</span></a></p>" : "<p>Your health data <strong>will not</strong> be used for research purposes | <a href='/F/app/05-make-your-choice' onclick='history.go(-1); return false;'>Edit</a></p>";
 
-  var planningPrefText = ( planningPref == "true" ) ?  "<p>Your health data <strong>will</strong> be shared for planning purposes | <a href='/F/app/05-make-your-choice' onclick='history.go(-1); return false;'>Edit <span class='util-visuallyhidden'>your planning preference</span></a></p>" : "<p>Your health data <strong>will not</strong> be shared for planning purposes | <a href='/F/app/05-make-your-choice' onclick='history.go(-1); return false;'>Edit</a></p>";
+  var planningPrefText = ( planningPref == "true" ) ?  "<p>Your health data <strong>will</strong> be used for planning purposes | <a href='/F/app/05-make-your-choice' onclick='history.go(-1); return false;'>Edit <span class='util-visuallyhidden'>your planning preference</span></a></p>" : "<p>Your health data <strong>can not</strong> be used for planning purposes | <a href='/F/app/05-make-your-choice' onclick='history.go(-1); return false;'>Edit</a></p>";
 
   $( '#your-planning-preferences' ).after( planningPrefText );
   $( '#your-research-preferences' ).after( researchPrefText );
