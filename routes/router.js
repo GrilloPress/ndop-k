fedb50c50b0e9a85015c9cce898c59b3fb42d2fe
grillopress <grillopress@gmail.com>
2018-03-02 10:26:32 +0000

    add k2 prototype

--- routes/router.js
+++ routes/router.js
@@ -39,6 +39,33 @@ router.get('/K/:subdir/:subdir2/:view', function(req, res, next) {
 // // // // // // //
 
 // GET v1 index page.
+router.get('/K2', function(req, res, next) {
+  res.render( 'K2/index' );
+});
+
+// GET all v1 URL reqs and push them to a template in the v1 file
+// This feels really brittle and hacky...
+// No handling of no view found...
+router.get('/K2/:view', function(req, res, next) {
+  var theView = req.params.view;
+  res.render( 'K2/' + theView );
+});
+
+router.get('/K2/:subdir/:view', function(req, res, next) {
+  var theView = req.params.view;
+  var theDir = req.params.subdir;
+  res.render( 'K2/' + theDir + '/' + theView );
+});
+
+router.get('/K2/:subdir/:subdir2/:view', function(req, res, next) {
+  var theView = req.params.view;
+  var theDir = req.params.subdir;
+  var theDir2 = req.params.subdir2;
+  res.render( 'K2/' + theDir + '/' + theDir2 + '/' + theView );
+});
+// // // // // // //
+
+// GET v1 index page.
 router.get('/v1', function(req, res, next) {
   res.render( 'v1/index' );
 });
