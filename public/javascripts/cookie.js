b54f002003ce4a0325f8fe17447485722d36ebcd
grillopress <grillopress@gmail.com>
2018-03-20 11:47:36 +0000

    fix minor wording error

--- public/javascripts/cookie.js
+++ public/javascripts/cookie.js
@@ -12,7 +12,7 @@ function createDiv(){
     var bodytag = document.getElementsByTagName('body')[0];
     var div = document.createElement('div');
     div.setAttribute('class','banner cookie');
-    div.innerHTML = '<div class="page-section">We use cookies to make the site simpler. <a href="' + PROTOTYPE_URL  + '/00-cookie-policy" target="_blank" rel="nofollow" title="Find out more about cookies">Find out more about cookies<span class="util-visuallyhidden"> - Page opens in new window</span></a><span aria-hidden="true"> (opens in new window)</span>.</div>';
+    div.innerHTML = '<div class="page-section">We use cookies to make this service simpler. <a href="' + PROTOTYPE_URL  + '/00-cookie-policy" target="_blank" rel="nofollow" title="Find out more about cookies">Find out more about cookies<span class="util-visuallyhidden"> - Page opens in new window</span></a><span aria-hidden="true"> (opens in new window)</span>.</div>';
 
     // bodytag.appendChild(div); // Adds the Cookie Law Banner just before the closing </body> tag
     // or
